9f046fd3785c08468468086220b09bd2b9f0a812
NVSSIM <nassim.joudar@datavalue-consulting.com>
2022-02-07 19:23:57 +0100

    implem part3 & test

---
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Game.java
@@ -0,0 +1,13 @@
+package fr.lernejo.fileinjector;
+
+public class Game {
+    public String title;
+    public String thumbnail;
+    public String short_description;
+    public String genre;
+    public String platform;
+    public String publisher;
+    public String developer;
+    public String release_date;
+    public Integer id;
+}
--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,33 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("only 1 parameter required");
+            return;
+        }
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
-
-            System.out.println("Hello after starting Spring");
+            Game[] games = new ObjectMapper().readValue(Paths.get(args[0]).toFile(), Game[].class);
+            RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+            rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+            for (Game game : games) {
+                rabbitTemplate.convertAndSend("", "game_info", game, m -> {
+                    m.getMessageProperties().getHeaders().put("game_id", game.id);
+                    return m;
+                });
+            }
         }
     }
 }
